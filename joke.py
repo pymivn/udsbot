1dd23a6458f5976f615ecfe24a16334bff15eaf4
Viet Hung Nguyen <hvn@familug.org>
2025-04-08 22:13:04 +0900

    Set temperature & top_p

--- joke.py
+++ joke.py
@@ -7,6 +7,7 @@ def gen_joke() -> str:
         "model": "gemma3",
         "prompt": "tell me a joke, add nothing else to the response, no emoji, max 240 chars",
         "stream": False,
+        "options": {"temperature": 5, "top_p": 0.7},
     }
     msg = requests.post(url, json=payload).json()["response"]
     return msg
