1b00985bad8e9abefbaff02a3c963125a9b60449
thevivotran <thevivotran@gmail.com>
2025-04-08 21:08:40 +0700

    Update joke.py setting

--- joke.py
+++ joke.py
@@ -4,10 +4,10 @@ import requests
 def gen_joke() -> str:
     url = "http://localhost:11434/api/generate"
     payload = {
-        "model": "gemma3",
+        "model": "gemma3:1b",
         "prompt": "tell me a joke, add nothing else to the response, no emoji, max 240 chars",
         "stream": False,
-        "options": {"temperature": 5, "top_p": 0.7},
+        "options": {"temperature": 0.8, "top_p": 0.9},
     }
     msg = requests.post(url, json=payload).json()["response"]
     return msg
